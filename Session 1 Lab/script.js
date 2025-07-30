f1ff45868a21c88ab3789de16c4549745fa9e918
Mohit Kumar <mohit951561@gmail.com>
2025-07-30 21:38:47 +0530

    Add files via upload

---
+++ Session 1 Lab/script.js
@@ -0,0 +1,33 @@
+// Question 1: 
+// Write a Node.js program that displays two separate messages in the console:
+// ● "Hello, this is my first Node.js program!"
+// ● "I am excited to learn more about Node.js!"
+// Then, modify the program to concatenate these two messages into a single line and display:
+// "Hello, this is my first Node.js program! I am excited to learn more about Node.js!"
+
+console.log("Hello, this is my first Node.js program!");
+console.log("I am excited to learn more about Node.js!");
+
+
+const message1 = "Hello, this is my first Node.js program!";
+const message2 = "I am excited to learn more about Node.js!";
+
+const combinedMessage = message1 + " " + message2;
+
+console.log(combinedMessage);
+
+// question no02
+// Write a program in Node.js to take any three sentences and print number of characters in each sentence separately.
+
+
+
+const sentence1 = "I love learning Node.js!";
+const sentence2 = "Node.js is great for backend development.";
+const sentence3 = "This is my third sentence.";
+
+console.log("Sentence 1 has", sentence1.length, "characters.");
+console.log("Sentence 2 has", sentence2.length, "characters.");
+console.log("Sentence 3 has", sentence3.length, "characters.");
+
+
+
